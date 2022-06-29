590c76f9fc32863a3b5569bd114e9b2e9f22c32c
Nemanja Dutina <nemanjadutina@gmail.com>
2022-06-29 15:37:50 +0200

    Created service for staff repository
    Created service for repository and service for staff conversion

---
+++ TollSystem/TollSystem/Core/Services/Users/IStaffModelService.cs
@@ -0,0 +1,10 @@
+﻿using TollSystem.Core.Entities;
+using TollSystem.Infrastructure.Models;
+
+namespace TollSystem.Core.Services.Users
+{
+    public interface IStaffModelService
+    {
+        public StaffEntity ModelToEntity(Staff s);
+    }
+}
