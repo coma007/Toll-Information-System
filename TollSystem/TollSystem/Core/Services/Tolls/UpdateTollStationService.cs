da08bb8a6fefc96d28be2211dd7952ab25e8cc5c
kaca01 <96604086+kaca01@users.noreply.github.com>
2022-06-29 23:41:42 +0200

    Create update toll station service

---
+++ TollSystem/TollSystem/Core/Services/Tolls/UpdateTollStationService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TollSystem.Core.Entities;
+
+namespace TollSystem.Core.Services.Tolls
+{
+    public class UpdateTollStationService
+    {
+        private IRepositoryService<TollStationEntity> _repository;
+        public UpdateTollStationService(IRepositoryService<TollStationEntity> repository)
+        {
+            _repository = repository;
+        }
+
+        public void UpdateTollStation(TollStationEntity tollStation)
+        {
+            TollStationEntity oldTollStation = _repository.GetById(tollStation.Id);
+            oldTollStation.Name = tollStation.Name;
+            oldTollStation.StationMaster = tollStation.StationMaster;
+            oldTollStation.Referents = tollStation.Referents;
+            _repository.Update(oldTollStation);
+            _repository.Save();
+        }
+    }
+}
