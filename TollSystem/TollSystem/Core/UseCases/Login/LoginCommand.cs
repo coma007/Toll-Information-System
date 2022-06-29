c28c937010639e381ff910e328c4fa2888cd7821
Milica Sladaković <milicasladakovic7@gmail.com>
2022-06-29 19:30:12 +0200

    Created Referent View and ViewModel

--- TollSystem/TollSystem/Core/UseCases/Login/LoginCommand.cs
+++ TollSystem/TollSystem/Core/UseCases/Login/LoginCommand.cs
@@ -1,17 +1,17 @@
-﻿using HealthInstitution.Commands;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TollSystem.Commands;
 using TollSystem.DesktopHost;
 using TollSystem.DesktopHost.Controllers;
 
-namespace TollSystem.Core.UseCases
+namespace TollSystem.Commands
 {
     public class LoginCommand : BaseCommand
     {
         public override void Execute(object parameter)
         {
-            NavigationStore.Instance().CurrentViewModel = new TollStationViewModel();
+            NavigationStore.Instance().CurrentViewModel = new ReferentViewModel();
         }
     }
 }
---
+++ TollSystem/TollSystem/DesktopHost/Controllers/MainPages/ReferentViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TollSystem.DesktopHost.Controllers
+{
+    public class ReferentViewModel : BaseViewModel
+    {
+        private string _ticketId;
+        public string TicketId
+        {
+            get { return _ticketId; }
+            set { _ticketId = value; OnPropertyChanged(nameof(TicketId)); }
+        }
+        private string _entrance;
+        public string Entrance
+        {
+            get { return _entrance; }
+            set { _entrance = value; OnPropertyChanged(nameof(Entrance)); }
+        }
+        private string _exit;
+        public string Exit
+        {
+            get { return _exit; }
+            set { _exit = value; OnPropertyChanged(nameof(Exit)); }
+        }
+        private string _entranceTime;
+        public string EntranceTime
+        {
+            get { return _entranceTime; }
+            set { _entranceTime = value; OnPropertyChanged(nameof(EntranceTime)); }
+        }
+
+        private string _exitTime;
+        public string ExitTime
+        {
+            get { return _exitTime; }
+            set { _exitTime = value; OnPropertyChanged(nameof(ExitTime)); }
+        }
+
+        private string _licencePlate;
+        public string LicencePlate
+        {
+            get { return _licencePlate; }
+            set { _licencePlate = value; OnPropertyChanged(nameof(LicencePlate)); }
+        }
+
+
+
+
+    }
+}
