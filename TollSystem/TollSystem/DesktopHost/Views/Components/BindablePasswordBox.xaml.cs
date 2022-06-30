9b127a2e7757943b07e1fd286d06ebeb0929a9dd
Milica Sladaković <milicasladakovic7@gmail.com>
2022-06-30 07:10:23 +0200

    Added login validation

---
+++ TollSystem/TollSystem/DesktopHost/Views/Components/BindablePasswordBox.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TollSystem.DesktopHost.Views
+{
+    /// <summary>
+    /// Interaction logic for BindablePasswordBox.xaml
+    /// </summary>
+    public partial class BindablePasswordBox : UserControl
+    {
+        /// <summary>
+        /// Interaction logic for BindablePasswordBox.xaml
+        /// </summary>
+        private bool _isPasswordChanging;
+
+        public static readonly DependencyProperty PasswordProperty =
+            DependencyProperty.Register("Password", typeof(string), typeof(BindablePasswordBox),
+                new FrameworkPropertyMetadata(string.Empty, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault,
+                    PasswordPropertyChanged, null, false, UpdateSourceTrigger.PropertyChanged));
+
+
+        public string Password
+        {
+            get { return (string)GetValue(PasswordProperty); }
+            set { SetValue(PasswordProperty, value); }
+        }
+
+        public BindablePasswordBox()
+        {
+            InitializeComponent();
+        }
+
+        private static void PasswordPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is BindablePasswordBox passwordBox)
+            {
+                passwordBox.UpdatePassword();
+            }
+        }
+
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            _isPasswordChanging = true;
+            Password = passwordBox.Password;
+            _isPasswordChanging = false;
+        }
+
+        private void UpdatePassword()
+        {
+            if (!_isPasswordChanging)
+            {
+                passwordBox.Password = Password;
+            }
+        }
+    }
+}
