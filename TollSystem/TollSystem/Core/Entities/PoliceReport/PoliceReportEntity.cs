21d6ce963c0b3afdc4797058db6f605dd307a893
kaca01 <96604086+kaca01@users.noreply.github.com>
2022-06-30 07:14:45 +0200

    Added police report entity

---
+++ TollSystem/TollSystem/Core/Entities/PoliceReport/PoliceReportEntity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TollSystem.Core.Entities
+{
+    public class PoliceReportEntity
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string LicensePlate { get; set; }
+        public double Speed { get; set; }
+
+        public PoliceReportEntity()
+        {
+
+        }
+
+        public PoliceReportEntity(int id, DateTime date, string licensePlate, double spped)
+        {
+            Id = id;
+            Date = date;
+            LicensePlate = licensePlate;
+            Speed = Speed;
+        }
+    }
+}
